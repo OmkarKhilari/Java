0117467b890611e8b8f7729894469d6165191aca
OmkarKhilari <omkarkhilari95@gmail.com>
2023-09-22 16:08:07 +0530

    Q-21 done

---
+++ src/Q_21.java
@@ -0,0 +1,60 @@
+public class Q_21 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return list1;
+        } else if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        ListNode ans = new ListNode();
+        ListNode tail = ans;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+
+        while (list1 != null) {
+            tail.next = list1;
+            list1 = list1.next;
+            tail = tail.next;
+        }
+
+        while (list2 != null) {
+            tail.next = list2;
+            list2 = list2.next;
+            tail = tail.next;
+        }
+
+        return ans.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    }
