6a2f3dd2c84dcd7d34fa66b5103e8d4ed1451672
OmkarKhilari <omkarkhilari95@gmail.com>
2023-09-22 16:06:35 +0530

    Q-141 done

---
+++ src/Q_141.java
@@ -0,0 +1,39 @@
+public class Q_141 {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+        static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
